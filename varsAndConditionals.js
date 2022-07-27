1e3011982a7eb9c43103f67b7522132839f3c0fa
Greg <gwcorbett@gmail.com>
2022-07-27 12:58:02 -0600

    First Commit

---
+++ varsAndConditionals.js
@@ -0,0 +1,28 @@
+/*
+    In this file, we'll be using variables, conditionals,
+    and loops to play out a fight between Jon Snow and
+    Jamie Lannister.
+*/ 
+
+let JonSnowAttack = 25
+let JamieLannisterAttack = 35
+
+// if (JonSnowAttack > JamieLannisterAttack){console.log('Jon has better attack!')}
+// else {console.log('Jamie has better attack!')}
+
+if (JonSnowAttack > JamieLannisterAttack) {console.log('Jon has better attack!')}
+else if (JamieLannisterAttack > JonSnowAttack){console.log('Jamie has better attack!')}
+else {console.log('Jon and Jamie have same attack!')}
+
+let JonSnowHealth = 100
+let JonSnowDefense = 0
+
+if (JonSnowHealth <= JamieLannisterAttack){console.log('Jon has been killed!')}
+else {JonSnowHealth = JonSnowHealth - JamieLannisterAttack console.log('Jon's health is down to ${JonSnowHealth}`)}
+
+JonSnowDefense += 25
+
+if (jonsnowhealth <= jamielannisterattack - jonsnowdefense) {console.log('Jon dies)}
+
+else {console.log('Jon's health is down to ${JonSnowHealth}`)}
+
